748f245cdda09680f4d5f8ca70caf589434283c3
alexcmoh <49571680+alexcmoh@users.noreply.github.com>
2020-09-02 23:56:59 -0400

    Updated index

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 function switchMenu(value)  {
-    
+    var primaryDiv = document.getElementById("mainContainer");
+
+    primaryDiv.innerHTML = "";
+
+    switch(value)   {
+        case 1:     // About me
+            var paragraph1 = document.createElement('span');
+            paragraph1.style = "font-size: 2em; margin-top: 5%";
+            paragraph1.className = "fadeInText";
+            paragraph1.appendChild(document.createTextNode("Hello! I am a test."));
+            primaryDiv.appendChild(paragraph1);
+            break;
+        default:
+            break;
+    }
 }
