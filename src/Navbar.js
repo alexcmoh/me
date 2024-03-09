d13bbc25ebf0d47725abf498f57ccd399594de43
Alex <alexcmoh@gmail.com>
2024-03-09 02:06:17 -0500

    Added auto-deploy

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,16 @@
+import './Navbar.css';
 import { Link } from "react-router-dom"
+
 const Navbar =()=>{
-      return (
-            <div>
-                  <Link to="/">Home</Link>
-            </div>
-      )
+	return (
+		<div className="sidebar-container">
+		<button className="menu-button">Hover Me</button>
+		<div className="sidebar-content">
+		<div className="menu-content">
+			<Link to="/">Home</Link>
+		</div>
+		</div>
+		</div>
+	)
 }
 export default Navbar;
