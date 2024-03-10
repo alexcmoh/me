7b29e02cf94d22e7e7403948f637ef49e026ef3c
Alex <alexcmoh@gmail.com>
2024-03-10 01:32:50 -0500

    Finished About page and begun Projects page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,10 @@
 import './Page.css';
 import IMGPurdueLogo from "../assets/images/Purdue-University-Logo.png";
 import IMGDeloitteLogo from "../assets/images/Deloitte-Logo.png";
+import IMGDiscordLogo from "../assets/images/Discord-Logo.png";
+import IMGKHUXSearchBanner from "../assets/images/KHUX-Search-Banner.png";
+import IMGProgrammingMashup from "../assets/images/Programming-Mashup.png";
+import IMGHackathonTeamPicture from "../assets/images/Hackathon-Team-Picture.png";
 
 function About() {
 	return (
@@ -8,23 +12,46 @@ function About() {
 			<h1>Welcome to Alex's portfolio.</h1>
 			<div className="divBlock">
 				<div className="halfBlock">
-					<p>Hello! My name is Alex Moh. I'm a Computer Science graduate from Purdue University. For the past 2 years, I have been working as a Software Engineer at Deloitte Consulting.</p>
-					<p>Hello! My name is Alex Moh. I'm a Computer Science graduate from Purdue University. For the past 2 years, I have been working as a Software Engineer at Deloitte Consulting.</p>
+					<p>Hello! My name is Alex Moh. I'm a Computer Science graduate from Purdue University. For the past 2 years, I have been gaining valuable hands-on experience working as a Software Engineer at Deloitte Consulting. I have also pursued further education in the form of certifications focused on cybersecurity and artificial intelligence.</p>
 				</div>
 				<div className="halfBlock">
-					<img className="imageFile" src={IMGPurdueLogo} alt="Image" />
-					<img className="imageFile" src={IMGDeloitteLogo} alt="Image" />
+					<img className="imageFile" src={IMGPurdueLogo} alt="Purdue University logo" />
+					<img className="imageFile" src={IMGDeloitteLogo} alt="Deloitte Consulting logo" />
 				</div>
 			</div>
-			<h1>Welcome to Alex's portfolio.</h1>
+			<br />
+			<br />
+			<br />
 			<div className="divBlock">
 				<div className="halfBlock">
-					<img className="imageFile" src={IMGPurdueLogo} alt="Image" />
+					<img className="imageFile size200" src={IMGDiscordLogo} alt="Discord logo" />
+					<img className="imageFile size400" src={IMGKHUXSearchBanner} alt="KHUX Info Page Sample" />
 				</div>
 				<div className="halfBlock">
-					<p>Sample Text</p>
+					<p>I have experience creating web apps and Python bots to serve communities. My most popular web app achieved over 70,000 views, and my best Python bot is deployed to Discord and used in servers containing tens of thousands of users. Being customer-driven is one of my core qualities.</p>
 				</div>
 			</div>
+			<br />
+			<br />
+			<br />
+			<div className="divBlock">
+				<div className="halfBlock">
+					<p>I leverage powerful languages such as Angular, React, HTML, and Python to create useful projects. I enjoy equal parts diving ahead on my own and collaborating in groups to achieve my goals.</p>
+				</div>
+				<div className="halfBlock">
+					<img className="imageFile size400" src={IMGProgrammingMashup} alt="Programming languages logos" />
+					<img className="imageFile size400" src={IMGHackathonTeamPicture} alt="Hackathon group picture" />
+				</div>
+			</div>
+			<br />
+			<br />
+			<br />
+			<div className="navbarDiv">
+				<p>Interested in working together? Click through the tabs on the top of the page to learn more about my initiatives!</p>
+			</div>
+			<br />
+			<br />
+			<br />
 		</div>
 	);
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,10 +1,38 @@
 import './Page.css';
+import IMGKHUXCostCalculator from "../assets/images/KHUX-Cost-Calculator.png";
+import IMGKHUXSearchOverview from "../assets/images/KHUX-Search-Overview.png";
+import IMGFFXIVGuidesHegemone from "../assets/images/FFXIV-Guides-Hegemone.png";
+import IMGFFXIVGuidesSildihn from "../assets/images/FFXIV-Guides-Sildihn.png";
 
 function Projects() {
 	return (
 		<div className="PageContent">
-			<h1>Projects Page</h1>
-			<p>This is the Projects page.</p>
+            <h1>KHUX Info Page</h1>
+			<div className="divBlock">
+				<div className="halfBlock">
+					<p>The KHUX Info Page is my first major personal project. In 2016, I delved into the mobile game Kingdom Hearts Union χ and quickly became a community resource for game data and information. However, as my challenges grew in complexity, I realized future endeavors would not be effectively solved by 280-character social media posts.</p>
+                    <p>This led to the creation of the KHUX Info Page, a website with multiple pages to support my data-sharing needs for the community. Since its initial launch with an upgrade cost calculator that spanned thousands of rows, the site has expanded to include tools for searching, sorting, and filtering over the 2,000 characters the game features. This unit search tool harnessed JavaScript and HTML in order to allow the user to perform operations over the dataset and view resulting characters in compact info cards.</p>
+                    <p>The overwhelmingly positive response took the shape of over 34,000 users and 77,000 visits, with users logging in from all over the world. This result was a gratifying payoff and inspired me to continue developing and maintaining the web app for community.</p>
+				</div>
+				<div className="halfBlock">
+					<img className="imageFile" src={IMGKHUXCostCalculator} alt="KHUX Info Page Cost Calculator" />
+					<img className="imageFile" src={IMGKHUXSearchOverview} alt="KHUX Info Page Search" />
+				</div>
+			</div>
+			<br />
+			<br />
+			<br />
+            <h1 className="headerRight">FFXIV Guides</h1>
+			<div className="divBlock">
+				<div className="halfBlock">
+					<img className="imageFile size500" src={IMGFFXIVGuidesHegemone} alt="FFXIV Guides example screenshot" />
+					<img className="imageFile size500" src={IMGFFXIVGuidesSildihn} alt="FFXIV Guides example screenshot" />
+				</div>
+				<div className="halfBlock">
+					<p>My next major website is for the massively multiplayer online game Final Fantasy 14. I organized several multi-person coordinated events where groups of 8 dedicated players participated in raids. In these raids, players must perform complex sequences of inputs, including executing an optimized rotation of actions and performing movements necessary to avoid boss attacks and losing the battle. In order to best communicate the boss's telegraphed attacks, the responsibilities of each player, and the strategies needed to weave all of these concepts together, I created another website.</p>
+                    <p>I created guides for each of the major fights in the game. Each guide was populated with images, videos, and text created by me. Over 50 players in my circle of raiders benefitted from my project. With this guide as their aid, many groups of players were able to successfully team up, clear challenges, and obtain achievements in the game that less than 10% of the playerbase own.</p>
+				</div>
+			</div>
 		</div>
 	);
 }
